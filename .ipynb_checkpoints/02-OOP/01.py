ab501e90a5696ae9390e58caa1a423d928ca557c
cp <cponecp@163.com>
2018-06-18 09:44:34 +0800

    By Carlous

---
+++ .ipynb_checkpoints/02-OOP/01.py
@@ -0,0 +1,20 @@
+class Student():
+    pass
+
+class LearningStu(n,a):
+    name = n
+    age = a
+    course = 'Python'
+
+    def thing(self):
+        print('%s would do homework'%name)
+        return None
+
+#cp = LearningStu()
+
+#print(cp.age)
+#cp.thing()
+
+cp1 = LearningStu(n='cp',a=28)
+cp1.thing()
+
